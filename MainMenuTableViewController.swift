a9d9b8b4e77278481cf1da49543fd4c1e57485e6
Demetrio <dem3perez@gmail.com>
2017-08-30 13:33:53 -0400

    Shows User Name
    Added a TableViewController to show the user's name once it is selected.

---
+++ MainMenuTableViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MainMenuTableViewController.swift
+//  Spellbook
+//
+//  Created by User on 8/30/17.
+//  Copyright © 2017 DemetrioPerez. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 4
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if let user = DataManager.instance.currentUser?.user
+        {
+            return "User \(user.name)"
+        }
+        else
+        {
+            return "Select User"
+        }
+        
+    }
+
+}
