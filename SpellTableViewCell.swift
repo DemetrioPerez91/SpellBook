7a71a8f959c2e267f9d18cdce2a8b0c90b29993e
Demetrio <dem3perez@gmail.com>
2017-09-01 09:53:09 -0400

    Add Cell xib for proyect

---
+++ SpellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SpellTableViewCell.swift
+//  
+//
+//  Created by User on 9/1/17.
+//
+//
+
+import UIKit
+
+class SpellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
