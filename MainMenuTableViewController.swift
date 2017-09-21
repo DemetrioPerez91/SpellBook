eb3a665214b30688df293dc033ca93363b824baf
Demetrio <dem3perez@gmail.com>
2017-09-21 17:47:00 -0400

    Add tutorial view and edit main storyboard

--- MainMenuTableViewController.swift
+++ MainMenuTableViewController.swift
@@ -13,6 +13,20 @@ class MainMenuTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.backgroundView = UIImageView(image: #imageLiteral(resourceName: "Papyrus"))
+        if  UserDefaults.standard.bool(forKey: "hasFinishedTutorial")
+        {
+           
+            
+        }
+        else
+        {
+            showToast(message: "YOU NEED TO DO THE TUTORIAL")
+            UserDefaults.standard.set(true, forKey: "hasFinishedTutorial")
+            performSegue(withIdentifier: "tutorial", sender: nil)
+            
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ TutorialViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TutorialViewController.swift
+//  Spellbook
+//
+//  Created by User on 9/21/17.
+//  Copyright © 2017 DemetrioPerez. All rights reserved.
+//
+
+import UIKit
+
+class TutorialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.isNavigationBarHidden = true
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+
+    }
+
+    @IBAction func okButton(_ sender: Any)
+    {
+        _ = navigationController?.popViewController(animated: true)
+         UserDefaults.standard.set(true, forKey: "hasFinishedTutorial")
+    }
+
+
+}
